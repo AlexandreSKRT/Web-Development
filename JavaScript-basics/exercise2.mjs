77b46c3e8510bf9052b0643672f9505c8f9e81de
Alexandre <alex.sukeratha@gmail.com>
2023-02-26 11:21:04 +0100

    getCount(), applyWordFunc(f), minCountWord() functions implemented, status : tested, end of ex2

--- JavaScript-basics/exercise2.mjs
+++ JavaScript-basics/exercise2.mjs
@@ -43,6 +43,33 @@ export class WordL {
           }
         results.sort();
         return results[0];
-        
+    }
+
+    minCountWord() {
+        var arr = Object.values(this.words_counted);
+        var min = Math.min(...arr);
+        var results = [];
+        for (var [key, value] of Object.entries(this.words_counted)) {
+            console.log(`${key}: ${value}`);
+            if (value === min) {
+                results.push(key);
+            }
+          }
+        results.sort();
+        return results[0];
+    }
+
+    getCount(word) {
+        if (this.text_split.includes(word)) {
+            return this.words_counted[word];
+        } else {
+            return 0;
+        }
+    }
+
+    applyWordFunc(f) {
+        var uniqueWords = this.getWords();
+        console.log(uniqueWords);
+        return uniqueWords.map(x => f(x));
     }
 }
--- JavaScript-basics/test_ex2.mjs
+++ JavaScript-basics/test_ex2.mjs
@@ -14,5 +14,20 @@ console.log("**** WordL class *****");
 var test = new WordL("fish bowl fish plane robot robot fish plane plane");
 console.log("-- Test getWords --");
 console.log(test.getWords());
+
 console.log("-- Test maxCountWord --");
 console.log("max occurences : " + test.maxCountWord());
+
+console.log("-- Test minCountWord --");
+console.log("min occurences : " + test.minCountWord());
+
+console.log("--- Test getCount --");
+console.log("Test for cheese : " + test.getCount("cheese"));
+console.log("Test for plane : " + test.getCount("plane"));
+
+console.log("--- Test applyWordFunc --");
+console.log("Test for .length : ")
+
+function f (word) {return word.length;}
+
+console.log(test.applyWordFunc(f))
