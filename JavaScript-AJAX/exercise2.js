5b92b15517a49e1271ad3936b6bc368112110294
Alexandre <alex.sukeratha@gmail.com>
2023-03-15 17:45:24 +0100

    send() function doesn't work; investigate php / XMLHttp request

--- JavaScript-AJAX/exercise2.js
+++ JavaScript-AJAX/exercise2.js
@@ -1,5 +1,18 @@
 "use strict";
 
 function send() {
+    window.alert('start');
     
+    var xhr = new XMLHttpRequest();
+    var input = document.getElementById("textedit").value;
+
+    var uri = "chat.php?phrase=" + input;
+    window.alert(uri);
+
+    xhr.open("GET", uri);
+    window.alert(xhr.responseText);
+    window.alert(xhr.responseType);
+
+    xhr.send();
+    window.alert('end');
 }
