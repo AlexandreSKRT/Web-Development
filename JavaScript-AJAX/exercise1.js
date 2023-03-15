9da609101c91c25171f2c23d10beb2f14eda922b
Alexandre <alex.sukeratha@gmail.com>
2023-03-15 13:53:49 +0100

    end of ex1,  status : tested

--- JavaScript-AJAX/exercise1.js
+++ JavaScript-AJAX/exercise1.js
@@ -5,7 +5,23 @@ function loadDoc() {
   xhr.open("GET", "text.txt");
   xhr.onload = function () {
     var text_area = document.getElementById("texta");
-    text_area.innerHTML = xhr.responseText;
+    text_area.innerHTML = this.responseText;
+  };
+  xhr.send();
+}
+
+function loadDoc2() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "text.txt");
+  xhr.onload = function () {
+    var text_area2 = document.getElementById("texta2");
+    var text_lines = this.responseText.split("<br/>");
+    for (var index in text_lines) {
+      var p = document.createElement("p");
+      p.innerHTML = text_lines[index];
+      p.style.color = "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
+      text_area2.appendChild(p);
+    }
   };
   xhr.send();
 }
