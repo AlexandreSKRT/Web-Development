9da546c13e27970336e93caba70992804df5587a
Alexandre <alex.sukeratha@gmail.com>
2023-03-29 16:35:24 +0200

    modify send() function

--- JavaScript-AJAX/exercise2.js
+++ JavaScript-AJAX/exercise2.js
@@ -1,18 +1,16 @@
 "use strict";
 
-function send() {
-    window.alert('start');
-    
+function send() {    
     var xhr = new XMLHttpRequest();
+    
     var input = document.getElementById("textedit").value;
-
     var uri = "chat.php?phrase=" + input;
-    window.alert(uri);
 
     xhr.open("GET", uri);
-    window.alert(xhr.responseText);
-    window.alert(xhr.responseType);
-
     xhr.send();
-    window.alert('end');
+
+    xhr.onload = function(){
+        alert(xhr.responseText);
+        alert(xhr.response);
+    }
 }
