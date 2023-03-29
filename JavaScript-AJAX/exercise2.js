97bb629788523afe81c9c24f27721b573602fd62
Alexandre <alex.sukeratha@gmail.com>
2023-03-29 17:05:43 +0200

    reload() function; status : Tested

--- JavaScript-AJAX/exercise2.js
+++ JavaScript-AJAX/exercise2.js
@@ -2,15 +2,46 @@
 
 function send() {    
     var xhr = new XMLHttpRequest();
-    
+
+    // Input uri
     var input = document.getElementById("textedit").value;
     var uri = "chat.php?phrase=" + input;
 
     xhr.open("GET", uri);
+    // [TESTING] : success || failure
+    xhr.onload = function(){
+        // alert(xhr.response);
+    }
+    xhr.send();
+}
+
+
+function reload() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "chatlog.txt");
     xhr.send();
 
     xhr.onload = function(){
-        alert(xhr.responseText);
-        alert(xhr.response);
+        // Retrieve text from "chatlog.txt"
+        var text_area = document.getElementById("texta");
+        var text_splitted = this.responseText.split("\n").reverse();
+
+        // Remove all child elements 
+        text_area.innerHTML = ""; 
+
+        // Add each line of text_splitted
+        for (let i=0; i<11; i++){
+            if (text_splitted[i] == ""){
+                continue;
+            } else {
+                var p = document.createElement("p");
+                p.textContent = text_splitted[i];
+                text_area.appendChild(p);
+            }
+        }
     }
+
+    xhr.send();
 }
+
+setInterval(reload, 1000);
