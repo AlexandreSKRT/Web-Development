2a72a9004ab2d7b840226ca6e33f7ad3b6c34e70
Alexandre <alex.sukeratha@gmail.com>
2023-04-18 00:24:24 +0200

    new version; switch case cleaned; need to filter tags and scripts for salut

--- Server/server.mjs
+++ Server/server.mjs
@@ -1,80 +1,67 @@
 "use strict";
 
 import { createServer } from "http";
-import querystring from "querystring";
 import * as fs from "fs";
 import mime from "mime";
+import querystring from "querystring";
 
 var listVisitors = [];
 
 // request processing
 function webserver(request, response) {
-  response.setHeader("Content-Type", "text/html; charset=utf-8");
+  console.log(request.url);
 
-  // Kill Process -> shuts down server
   if (request.url == "/kill") {
-    response.end(
-      "<!doctype html><html><body>The server will stop now.</body></html>"
-    );
+    // Kill Process -> shuts down server
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    response.end("<!doctype html><html><body>The server will stop now.</body></html>");
     process.exit(0);
 
-    // GET request -> 'hi?user='
-  } else if (request.url.includes("/hi?nom=")) {
+  } else if (request.url == "/clear") {
+    // Clear visitors historic
+    listVisitors = [];
     response.setHeader("Content-Type", "text/html; charset=utf-8");
-    //console.log(request.url); spaces are translated to + ?
+    response.end("<!doctype html><html><body>The visitors historic has been cleaned.</body></html>");
+
+  } else if (request.url.startsWith("/hi?nom=")){
+    // GET request -> "/hi?nom="
     var name = querystring.unescape(request.url.substr(8));
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
     response.end("<!doctype html><html><body> hi " + name + "</body></html>");
 
-    // GET request -> 'salut?visiteur='
-  } else if (request.url.includes("/salut?visiteur=")) {
-    response.setHeader("Content-Type", "text/html; charset=utf-8");
+  } else if (request.url.startsWith("/salut?visiteur=")) {
+    // GET request -> "/salut?visiteur="
     var visitor = querystring.unescape(request.url.substr(16));
-    response.end(
-      "<!doctype html><html><body>salut " +
-        visitor +
-        ", the following users have already visited this page: " +
-        listVisitors.join(", ") +
-        "</body></html>"
-    );
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    response.end("<!doctype html><html><body>salut " + visitor + ", the following users have already visited this page: " + listVisitors.join(", ") + "</body></html>");
     listVisitors.push(visitor);
-  } else if (request.url == "/clear") {
-    listVisitors = [];
-    response.end(
-      "<!doctype html><html><body>The visitors historic has been cleaned.</body></html>"
-    );
 
-    // Handle files request
-  } else {
-    const req_path = request.url.slice(1);
-    console.log(req_path);
-
-    // Check that the server is not serving files in parent directories
-    if (req_path.includes("..")) {
-      response.statusCode = 404;
-      response.end(
-        "<!doctype html><html><body>The server is not serving files in parent directories.</body></html>"
-      );
+  } else if (request.url.startsWith("/www/")) {
+    // Handle files
+    var required_path = request.url.substr(5);
+    // console.log(required_path);
+    if(fs.existsSync(required_path)){
+        response.setHeader("Content-Type", mime.getType(required_path));  
+        response.end(fs.readFileSync(required_path));
+    }
+    else if(required_path.includes('..')){
+        response.statusCode=404;
+        response.end("<!doctype html><html><body>The server is not serving files in parent directories.</body></html>"); 
+    }
+    else{
+        response.statusCode=404;
+        response.end("<!doctype html><html><body>File does not exist.</body></html>");
     }
 
-    // Check if the file exists inside the parent directory
-    fs.access(req_path, (err) => {
-      if (err) {
-        // Send error statusCode 404
-        response.statusCode = 404;
-        response.end(
-          "<!doctype html><html><body>File not found.</body></html>"
-        );
-      } else {
-        // Send File
-        response.setHeader("Content-Type", mime.getType(req_path));
-        response.end(fs.readFileSync(req_path));
-      }
-    });
+  } else {
+    // Server works! 
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    response.end("<!doctype html><html><body>Server works!</body></html>");
   }
 }
-
-// create server object
-const server = createServer(webserver);
-
-// server listens
-server.listen(process.argv[2], (err) => {});
+try {
+  // create server object
+  const server = createServer(webserver);
+  // server listens
+  server.listen(process.argv[2], (err) => {});
+} catch (e) {}
