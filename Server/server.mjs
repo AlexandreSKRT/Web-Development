b048247e1cb7dd3bbee5768bc2ef4bac201724f5
Alexandre <alex.sukeratha@gmail.com>
2023-04-17 22:56:00 +0200

    storing visitors; status : tested

--- Server/server.mjs
+++ Server/server.mjs
@@ -5,22 +5,30 @@ import querystring from "querystring";
 import * as fs from "fs";
 import mime from "mime";
 
+var listVisitors = []; 
+
 // request processing
 function webserver(request, response) {
   response.setHeader("Content-Type", "text/html; charset=utf-8");
 
   // Kill Process -> shuts down server
   if (request.url == "/kill") {
-    response.end(
-      "<!doctype html><html><body>The server will stop now.</body></html>"
-    );
+    response.end("<!doctype html><html><body>The server will stop now.</body></html>");
     process.exit(0);
 
   // GET request -> 'hi?user='
   } else if (request.url.includes('/hi?nom=')) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
-    var name = querystring.unescape(request.url.substr(9));
+    //console.log(request.url); spaces are translated to + ?
+    var name = querystring.unescape(request.url.substr(8));
     response.end("<!doctype html><html><body> hi " + name + "</body></html>");
+
+  // GET request -> 'salut?visiteur='
+  } else if (request.url.includes('/salut?visiteur=')) { 
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    var visitor = querystring.unescape(request.url.substr(16));
+    response.end("<!doctype html><html><body>salut " + visitor + ", the following users have already visited this page: " + listVisitors.join(", ") + "</body></html>");
+    listVisitors.push(visitor);
 
   // Handle files request
   } else {
@@ -30,9 +38,7 @@ function webserver(request, response) {
     // Check that the server is not serving files in parent directories
     if (req_path.includes("..")) {
       response.statusCode = 404;
-      response.end(
-        "<!doctype html><html><body>The server is not serving files in parent directories.</body></html>"
-      );
+      response.end("<!doctype html><html><body>The server is not serving files in parent directories.</body></html>");
     }
 
     // Check if the file exists inside the parent directory
@@ -40,9 +46,7 @@ function webserver(request, response) {
       if (err) {
         // Send error statusCode 404
         response.statusCode = 404;
-        response.end(
-          "<!doctype html><html><body>File not found.</body></html>"
-        );
+        response.end("<!doctype html><html><body>File not found.</body></html>");
       } else {
         // Send File
         response.setHeader("Content-Type", mime.getType(req_path));
