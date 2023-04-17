f1f28d6650cedc56518eb0f5d4906de647793fa4
Alexandre <alex.sukeratha@gmail.com>
2023-04-17 21:43:59 +0200

    Get Request + handel file request; status: tested

--- Server/server.mjs
+++ Server/server.mjs
@@ -1,17 +1,48 @@
 "use strict";
 
-import {createServer} from "http";
+import { createServer } from "http";
+import * as fs from "fs";
+import mime from "mime";
 
 // request processing
-function webserver( request, response ) {
-    response.setHeader("Content-Type", "text/html; charset=utf-8");  
+function webserver(request, response) {
+  response.setHeader("Content-Type", "text/html; charset=utf-8");
 
-    if (request.url == "/kill"){
-        response.end("<!doctype html><html><body>The server will stop now.</body></html>"); 
-        process.exit(0);
+  // Kill Process -> shuts down server
+  if (request.url == "/kill") {
+    response.end("<!doctype html><html><body>The server will stop now.</body></html>");
+    process.exit(0);
+  // GET request -> 'hi?user='
+  } else if (request.url.slice(0, 9) == "/hi?user=") {
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    var name = querystring.unescape(request.url.substr(9));
+    response.end("<!doctype html><html><body> hi " + name + "}</body></html>");
+  // Handle files request
+  } else {
+    const req_path = request.url.slice(1);
+    console.log(req_path);
+
+    // Check that the server is not serving files in parent directories
+    if (req_path.includes("..")) {
+      response.statusCode = 404;
+      response.end(
+        "<!doctype html><html><body>The server is not serving files in parent directories.</body></html>"
+      );
     }
 
-    response.end("<!doctype html><html><body>Server works.</body></html>");
+    // Check if the file exists inside the parent directory
+    fs.access(req_path, (err) => {
+      if (err) {
+        response.statusCode = 404;
+        response.end(
+          "<!doctype html><html><body>File not found.</body></html>"
+        );
+      } else {
+        response.setHeader("Content-Type", mime.getType(req_path));
+        response.end(fs.readFileSync(req_path));
+      }
+    });
+  }
 }
 
 // create server object
