7dfd3e0ba694b337babcb2e59fe24a9bf11d48c4
Alexandre <alex.sukeratha@gmail.com>
2023-04-17 23:09:09 +0200

    clean visitors historic option; status: tested

--- Server/server.mjs
+++ Server/server.mjs
@@ -5,7 +5,7 @@ import querystring from "querystring";
 import * as fs from "fs";
 import mime from "mime";
 
-var listVisitors = []; 
+var listVisitors = [];
 
 // request processing
 function webserver(request, response) {
@@ -13,24 +13,37 @@ function webserver(request, response) {
 
   // Kill Process -> shuts down server
   if (request.url == "/kill") {
-    response.end("<!doctype html><html><body>The server will stop now.</body></html>");
+    response.end(
+      "<!doctype html><html><body>The server will stop now.</body></html>"
+    );
     process.exit(0);
 
-  // GET request -> 'hi?user='
-  } else if (request.url.includes('/hi?nom=')) {
+    // GET request -> 'hi?user='
+  } else if (request.url.includes("/hi?nom=")) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
     //console.log(request.url); spaces are translated to + ?
     var name = querystring.unescape(request.url.substr(8));
     response.end("<!doctype html><html><body> hi " + name + "</body></html>");
 
-  // GET request -> 'salut?visiteur='
-  } else if (request.url.includes('/salut?visiteur=')) { 
+    // GET request -> 'salut?visiteur='
+  } else if (request.url.includes("/salut?visiteur=")) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
     var visitor = querystring.unescape(request.url.substr(16));
-    response.end("<!doctype html><html><body>salut " + visitor + ", the following users have already visited this page: " + listVisitors.join(", ") + "</body></html>");
+    response.end(
+      "<!doctype html><html><body>salut " +
+        visitor +
+        ", the following users have already visited this page: " +
+        listVisitors.join(", ") +
+        "</body></html>"
+    );
     listVisitors.push(visitor);
+  } else if (request.url == "/clear") {
+    listVisitors = [];
+    response.end(
+      "<!doctype html><html><body>The visitors historic has been cleaned.</body></html>"
+    );
 
-  // Handle files request
+    // Handle files request
   } else {
     const req_path = request.url.slice(1);
     console.log(req_path);
@@ -38,7 +51,9 @@ function webserver(request, response) {
     // Check that the server is not serving files in parent directories
     if (req_path.includes("..")) {
       response.statusCode = 404;
-      response.end("<!doctype html><html><body>The server is not serving files in parent directories.</body></html>");
+      response.end(
+        "<!doctype html><html><body>The server is not serving files in parent directories.</body></html>"
+      );
     }
 
     // Check if the file exists inside the parent directory
@@ -46,7 +61,9 @@ function webserver(request, response) {
       if (err) {
         // Send error statusCode 404
         response.statusCode = 404;
-        response.end("<!doctype html><html><body>File not found.</body></html>");
+        response.end(
+          "<!doctype html><html><body>File not found.</body></html>"
+        );
       } else {
         // Send File
         response.setHeader("Content-Type", mime.getType(req_path));
